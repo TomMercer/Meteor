5548d127bfcd5282e6f3a3d5bce630a7c5690aca
InTheLittleWood <dragon@dugar.co.uk>
2014-07-28 19:18:40 +0100

    Everything

--- meteorapp.js
+++ meteorapp.js
@@ -1,19 +1,7 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteorapp.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+Template.main.events = {
+'click #modal-trigger' : function (event){
+  event.preventDefault();
+  $('#modal-id').modal();
+  console.log("Test")
 }
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
 }
