56c0e479901b68d5d8e8463073a74251972aa7ff
JoeSquishy <Joe.qureshi@icloud.com>
2014-07-28 21:13:20 +0100

    Formatted HTML & fixed missing <Div> tags & fixed Javascript methods back up

--- meteorapp.js
+++ meteorapp.js
@@ -1,7 +1,21 @@
-Template.main.events = {
+
+
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to meteorapp.";
+  };
+
+  Template.main.events = {
 'click #modal-trigger' : function (event){
   event.preventDefault();
   $('#modal-id').modal();
   console.log("Test")
 }
+}
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
 }
