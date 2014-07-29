4ad7d442301695cadb5baece6a9799c9c837760e
JoeSquishy <Joe.qureshi@icloud.com>
2014-07-29 12:15:08 +0100

    Fixed Conflicts - Ty Neil

--- meteorapp.js
+++ meteorapp.js
@@ -1,19 +1,22 @@
 if (Meteor.isClient) {
-<<<<<<< HEAD
-    Template.main.events = {
-        'click #modal-trigger': function(event) {
-            event.preventDefault();
-            $('#modal-id').modal();
-            console.log("Test")
-        }
+      Template.main.events({
+    'click #modal-trigger': function(event) {
+      event.preventDefault();
+      $('#modal-id').modal();
     }
+  });
+  Template.main.rendered = function() {
+    $('#modal-id').modal();
+  }
 }
 
-    if (Meteor.isServer) {
-        Meteor.startup(function() {
-            console.log("go suck a nut.")
-        });
-    }
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    // code to run on server at startup
+  });
+}
+
 
     function set_current_location() {
         if (navigator.geolocation) {
@@ -35,22 +38,3 @@ if (Meteor.isClient) {
     function find_google_places(){
     
     }
-=======
-  Template.main.events({
-    'click #modal-trigger': function(event) {
-      event.preventDefault();
-      $('#modal-id').modal();
-    }
-  });
-  Template.main.rendered = function() {
-    $('#modal-id').modal();
-  }
-}
-
-
-if (Meteor.isServer) {
-  Meteor.startup(function() {
-    // code to run on server at startup
-  });
-}
->>>>>>> 9b759bc17eff89b2cc9e9c8c2d1d06f1bc017d5a
