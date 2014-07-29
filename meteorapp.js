1b6e0f07a80e264dc82889c19e14b6416214ce97
JoeSquishy <Joe.qureshi@icloud.com>
2014-07-29 10:47:41 +0100

    Clean Up JS

--- meteorapp.js
+++ meteorapp.js
@@ -1,21 +1,16 @@
-
-
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteorapp.";
-  };
 
-  Template.main.events = {
-'click #modal-trigger' : function (event){
-  event.preventDefault();
-  $('#modal-id').modal();
-  console.log("Test")
-}
-}
+    Template.main.events = {
+        'click #modal-trigger': function(event) {
+            event.preventDefault();
+            $('#modal-id').modal();
+            console.log("Test")
+        }
+    }
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        console.log("go suck a nut.")
+    });
 }
